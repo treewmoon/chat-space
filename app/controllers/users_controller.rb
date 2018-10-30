ebdf9529b50f49365c2192aa556934721b0dea1c
treewmoon <tech-camp@tech-camp.local>
2018-10-30 14:21:10 +0900

    ユーザー情報編集機能の追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def edit
+  end
+
+
+# current_user.updateに成功 → rootにリダイレクト
+# 失敗 → editのビューを再度描画
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
