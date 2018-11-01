58f2bd77ba1edc9ceb852dadb69bf6046399f6a4
treewmoon <tech-camp@tech-camp.local>
2018-11-01 21:25:35 +0900

    微調整

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :message do
     content Faker::Lorem.sentence
-    image File.open("#{Rails.root}/public/images/no_image.jpg")
+    image File.open("#{Rails.root}/public/test/test_image.jpg")
     user
     group
   end
