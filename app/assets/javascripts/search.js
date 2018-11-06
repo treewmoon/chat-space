3ffc0d0e897a7c86931564cfce87c1a73f05abc6
treewmoon <tech-camp@tech-camp.local>
2018-11-06 19:27:07 +0900

    イベント時に非同期通信

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,6 +1,11 @@
 $(function() {
   $('#user-search-field').on("keyup", function() {
     var input = $('#user-search-field').val();
-    console.log(input)
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
   });
 });
