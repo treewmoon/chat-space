401834af96b1e5f20e28d18280f3809ea1dfdd0a
treewmoon <tech-camp@tech-camp.local>
2018-11-08 15:16:44 +0900

    setIntervalの使用

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
-$(function(){
-   function buildHTML(message){
-      var image = message.image_url ? `<img src="${message.image_url}" class='lower-message__image'>` : "";
+$(document).on('turbolinks:load', function(){
+  function buildHTML(message){
+    var image = message.image_url ? `<img src="${message.image_url}" class='lower-message__image'>` : "";
     var html = `<div class="message">
                   <div class="upper-message">
                     <div class="upper-message__user-name">
@@ -36,12 +36,33 @@ $(function(){
       var html = buildHTML(data);
       $('.messages').append(html);
       $('.form__message').val('');
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'slow');
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
       $('.form__submit').prop("disabled", false);
     })
     .fail(function(){
       alert('メッセージの投稿に失敗しました。');
     })
   })
+
+  var interval = setInterval(function(){
+    var lastMessageId = $('.message').last().attr('message-id');
+    var presentHTML = window.location.href;
+    if (presentHTML.match(/\/groups\/\d+\/messages/)) {
+      console.log(lastMessageId)
+      $.ajax ({
+        url: presentHTML,
+        type: 'GET',
+        data: {id: lastMessageId},
+        dataType: 'json',
+      })
+      .done(function(json){
+      })
+      .fail(function() {
+        alert('');
+      })
+    } else {
+      clearInterval(interval)
+    }
+  },5000);
 })
 
